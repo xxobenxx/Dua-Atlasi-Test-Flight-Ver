c438697c946d96bb49000f6782ccdea58c8cfd96
Oben <144821542+xxobenxx@users.noreply.github.com>
2023-12-19 15:44:25 +0100

    finalApp
    local data file implemented, search function works
    play sound will be implemented

--- pages/Home.js
+++ pages/Home.js
@@ -1,106 +1,88 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, StyleSheet, SafeAreaView, Button, ScrollView, Keyboard, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import { SearchBar } from 'react-native-elements';
-import axios from 'axios';
-import { Audio } from 'expo-av';
+import Clipboard from '@react-native-community/clipboard';
+
+
+import {dataWithMeaning} from '../data/data_with_meaning';
 
 const Home = () => {
-  const [keyword, setKeyword] = useState('');
+  const [searchText, setSearchText] = useState('');
   const [matchingVerses, setMatchingVerses] = useState([]);
 
-  const handleChange = (text) => setKeyword(text);
+  const handleChange = (text) => setSearchText(text);
 
-  const handleSubmit = async() => {
-    try {
-      // const res = await axios.get("https://api.acikkuran.com/surahs")
-      // console.log(res);
-      let DATA = []
-       for(let i = 1; i < 3; i++) {
-        console.log("syrah count",i);
-        const apiUrl = `https://api.acikkuran.com/surah/${i}?author=8`;
-        const res = await axios.get(apiUrl);
-        for(let j = 0 ; j < res.data.data.verses.length; j++) {
-let verseObj = {...res.data.data.verses[j], surah_audio:res.data.data.audio.mp3}
-          DATA.push(verseObj);
-}}
-DATA.length = 5
-console.log(DATA);
-      //   }
-    } catch (error) {
-      console.log(error);
-    }
-    // setLoading(true);
-    // handleSearch();
-    // Keyboard.dismiss();
+  const handleSearch = () => {
+    const keyword = searchText.trim().toLowerCase();
+  
+    const foundVerses = dataWithMeaning.filter(
+      (verse) =>
+        verse.meaning.toLowerCase().includes(keyword) 
+    );
+
+    console.log('Search keyword:', keyword);
+  console.log('Found Verses:', foundVerses);
+  
+    setMatchingVerses(foundVerses);
   };
 
-  const handleSearch = async () => {
-    const keyword = keyword.trim();
+  const handleVerseSelection = (selectedVerse) => {
+    const clipboardContent = `Sure: ${selectedVerse.verse}\nAnlamı: ${
+      selectedVerse.translation.text || 'Not available'
+    }`;
 
-    try {
- 
-      console.log("api res",res);
-      const { data } = res.data;
-
-      const foundVerses = findVersesWithKeyword(data, keyword);
-
-      console.log('foundVerses:', foundVerses);
-
-      if (foundVerses.length > 0) {
-        setMatchingVerses(foundVerses);
-      } else {
-        setMatchingVerses(['ilgili dua bulunamadı']);
-      }
-
-      setIsReady(true);
-      setError('');
-    } catch (error) {
-      setError(error.message);
-      setIsReady(true);
-    } finally {
-      setLoading(false);
-    }
+    Clipboard.setString(clipboardContent)
+      .then(() => {
+        alert('Selected verse copied to clipboard');
+      })
+      .catch((error) => {
+        console.error('Error copying to clipboard:', error);
+      });
   };
-
-  // const findVersesWithKeyword = (data, keyword) => {
-  //   const lowerCaseKeyword = keyword.toLowerCase();
-  //   return data.verses.filter((verse) =>
-  //     verse.translation.text.toLowerCase().includes(lowerCaseKeyword)
-  //   );
-  // };
+  
 
   return (
     <SafeAreaView>
       <Text style={styles.header}>SEARCH AND SEND</Text>
+
       <View style={styles.container}>
         <SearchBar
-          placeholder="Aradığınız kelime"
+          placeholder="Search for verse or meaning"
           onChangeText={handleChange}
-          value={keyword}
+          value={searchText}
           lightTheme={true}
           round
           cancelIcon={true}
+          
         />
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+
+        <TouchableOpacity
+          onPress={handleSearch}
+          style={styles.buttonContainer}
+        >
+          <View style={styles.button}>
             <Text style={styles.buttonText}>Dua Bul / Find</Text>
-          </TouchableOpacity>
-        </View>
+          </View>
+        </TouchableOpacity>
+
         <ScrollView style={styles.resultsContainer}>
           {matchingVerses.map((verse, index) => (
-            <View key={index} style={styles.verseContainer}>
-              <Text style={styles.verseText}>
-                Sure: {verse.verse}
-              </Text>
-              {verse.translation && verse.translation.text ? (
-                <Text style={styles.verseText}>
-                  Anlamı: {verse.translation.text}
-                </Text>
-              ) : (
-                <Text style={styles.verseText}>Aradığınız kelime bulunamadı</Text>
-              )}
-              <Button onPress={playSound} title="Play Sound" />
-            </View>
+            <TouchableOpacity
+              key={index}
+              onPress={() => handleVerseSelection(verse)}
+              style={styles.verseContainer}
+            >
+              <Text style={styles.verseText}>Verse: {verse.verse}</Text>
+              <Text style={styles.verseText}>Translation: {verse.translation.text}</Text>
+              <Text style={styles.verseText}>Audio: {verse.surah_audio}</Text>
+            </TouchableOpacity>
           ))}
         </ScrollView>
       </View>
@@ -115,16 +97,16 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
   },
-  container: {},
-  results: {
-    marginTop: 10,
-    fontSize: 18,
-    textAlign: 'center',
+
+  container: {
+    margin: 10,
   },
+
   buttonContainer: {
     alignItems: 'center',
     marginTop: 10,
   },
+
   button: {
     borderWidth: 1,
     borderColor: 'black',
@@ -133,18 +115,22 @@ const styles = StyleSheet.create({
     marginTop: 10,
     backgroundColor: '#208796',
   },
+
   buttonText: {
     fontSize: 18,
     textAlign: 'center',
     color: 'gold',
   },
+
   verseContainer: {
     borderBottomWidth: 1,
     borderColor: 'gray',
+    marginVertical: 5,
   },
+
   verseText: {
     fontSize: 16,
-    marginBottom: 8,
+    marginVertical: 5,
   },
 });
 
